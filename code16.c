9259c0b2d40126253a90300b0765a0c1bb44067f
niharikaty <niharikaty07@gmail.com>
2025-09-03 22:25:55 +0530

    Add files via upload

---
+++ code16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float principle,rate,time;
+    printf("enter principle,rate and time");
+    scanf("%f %f %f",&principle,&rate,&time);
+    float simple=((principle*rate*time)/100.0);
+    float comp=principle*pow(1+(rate/100.0),time)-principle;
+    printf("simple interest=%f and compound interest=%f",simple,comp);
+    return 0
+}
